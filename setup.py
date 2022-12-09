4012e3cfc187f2839bb40172f3d82794fe73358a
David <david26694@gmail.com>
2022-12-09 13:50:39 +0100

    release 033

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ dev_packages = test_packages + util_packages + docs_packages
 
 setup(
     name="cluster_experiments",
-    version="0.3.2",
+    version="0.3.3",
     packages=find_packages(),
     extras_require={
         "dev": dev_packages,
