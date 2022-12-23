83e333d11e8506713fe5ebbcc18d84dbb930ef6e
David <david26694@gmail.com>
2022-12-23 16:13:58 +0100

    refactor tests

--- tests/splitter/test_time_col.py
+++ tests/splitter/test_time_col.py
@@ -38,17 +38,38 @@ def minute_df():
 
 
 @pytest.mark.parametrize(
-    "switchback_freq,n_splits",
-    [("1D", 10), ("2D", 5), ("4D", 3)],
+    "df,switchback_freq,n_splits",
+    [
+        ("date_df", "1D", 10),
+        ("date_df", "2D", 5),
+        ("date_df", "4D", 3),
+        ("hourly_df", "H", 240),
+        ("hourly_df", "2H", 120),
+        ("hourly_df", "4H", 60),
+        ("hourly_df", "6H", 40),
+        ("hourly_df", "12H", 20),
+        ("minute_df", "min", 14400),
+        ("minute_df", "2min", 7200),
+        ("minute_df", "4min", 3600),
+        ("minute_df", "30min", 480),
+        ("minute_df", "60min", 240),
+        ("yearly_df", "W", 53),
+        ("yearly_df", "M", 12),
+    ],
 )
 @switchback_splitter_parametrize
-def test_date_col(splitter, date_df, switchback_freq, n_splits, request):
+def test_date_col(splitter, df, switchback_freq, n_splits, request):
+    date_df = request.getfixturevalue(df)
     splitter = request.getfixturevalue(splitter)
     splitter.switch_frequency = switchback_freq
     time_col = splitter._get_time_col(date_df)
     assert time_col.dtype == "datetime64[ns]"
-    pd.testing.assert_series_equal(time_col, date_df["time"].dt.floor(switchback_freq))
     assert time_col.nunique() == n_splits
+
+    if "W" not in switchback_freq and "M" not in switchback_freq:
+        pd.testing.assert_series_equal(
+            time_col, date_df["time"].dt.floor(switchback_freq)
+        )
 
 
 @pytest.mark.parametrize(
@@ -74,57 +95,3 @@ def test_week_col_date(splitter, date_df, switchback_freq, day_of_week, request)
     assert (
         time_col.dt.day_name().iloc[0] == day_of_week
     ), f"{switchback_freq} failed, day_of_week is {time_col.dt.day_name().iloc[0]}"
-
-
-@pytest.mark.parametrize(
-    "switchback_freq,n_splits",
-    [
-        ("M", 12),
-        ("W", 53),
-    ],
-)
-@switchback_splitter_parametrize
-def test_week_col(splitter, yearly_df, switchback_freq, n_splits, request):
-    splitter = request.getfixturevalue(splitter)
-    splitter.switch_frequency = switchback_freq
-    time_col = splitter._get_time_col(yearly_df)
-    assert time_col.dtype == "datetime64[ns]"
-    assert time_col.nunique() == n_splits, f"nunique is {time_col.nunique()}"
-
-
-@pytest.mark.parametrize(
-    "switchback_freq,n_splits",
-    [
-        ("H", 240),
-        ("2H", 120),
-        ("4H", 60),
-        ("6H", 40),
-        ("12H", 20),
-    ],
-)
-@switchback_splitter_parametrize
-def test_hourly_col(splitter, hourly_df, switchback_freq, n_splits, request):
-    splitter = request.getfixturevalue(splitter)
-    splitter.switch_frequency = switchback_freq
-    time_col = splitter._get_time_col(hourly_df)
-    assert time_col.dtype == "datetime64[ns]"
-    assert time_col.nunique() == n_splits, f"nunique is {time_col.nunique()}"
-
-
-@pytest.mark.parametrize(
-    "switchback_freq,n_splits",
-    [
-        ("min", 14400),
-        ("2min", 7200),
-        ("4min", 3600),
-        ("30min", 480),
-        ("60min", 240),
-    ],
-)
-@switchback_splitter_parametrize
-def test_minute_col(splitter, minute_df, switchback_freq, n_splits, request):
-    splitter = request.getfixturevalue(splitter)
-    splitter.switch_frequency = switchback_freq
-    time_col = splitter._get_time_col(minute_df)
-    assert time_col.dtype == "datetime64[ns]"
-    assert time_col.nunique() == n_splits, f"nunique is {time_col.nunique()}"
