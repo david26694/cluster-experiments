3621c4b5956535202db386d6ff05d68c9ce23ac5
David Masip <david26694@gmail.com>
2023-01-25 16:40:04 +0100

    docs typo (#78)

--- cluster_experiments/random_splitter.py
+++ cluster_experiments/random_splitter.py
@@ -413,6 +413,7 @@ class StratifiedSwitchbackSplitter(StratifiedClusteredSplitter, SwitchbackSplitt
     df = pd.DataFrame({"date": ["2020-01-01", "2020-01-02", "2020-01-03","2020-01-04"], "country":["C1","C2","C2","C1"]})
     df = splitter.assign_treatment_df(df)
     print(df)
+    ```
     """
 
     def __init__(
