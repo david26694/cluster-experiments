b1551385af0430e5e5a8ff10e8ca354a56b566a7
Gabriel Ristow Cidral <33734080+Gabrielcidral1@users.noreply.github.com>
2023-02-08 12:51:09 +0100

    implement paired t test (#80)
    * feat: first commit to implement paired t test
    
    * fix: pivot comparison col
    
    * refactor: add better assert
    
    * fix: david's comments
    
    * fix: strata_cols as subset of cluster, new tests
    
    * fix: treat nas
    
    * feat: add isort
    
    * add cluster to usage
    
    * pr review
    
    * adding to init, mkfile, docs
    
    * fix: optional verbose
    
    * fix: remove empty test, add description to readme
    
    * refactor: asserts of dtypes
    
    * feat: add example
    
    * add codecov version
    
    ---------
    
    Co-authored-by: David <david26694@gmail.com>

---
+++ examples/short_example_paired_ttest.py
@@ -0,0 +1,53 @@
+from datetime import date
+
+import numpy as np
+import pandas as pd
+
+from cluster_experiments.experiment_analysis import PairedTTestClusteredAnalysis
+from cluster_experiments.perturbator import UniformPerturbator
+from cluster_experiments.power_analysis import PowerAnalysis
+from cluster_experiments.random_splitter import StratifiedSwitchbackSplitter
+
+
+def generate_random_data(clusters, dates, N):
+    # Generate random data with clusters and target
+    users = [f"User {i}" for i in range(1000)]
+    df = pd.DataFrame(
+        {
+            "cluster": np.random.choice(clusters, size=N),
+            "target": np.random.normal(0, 1, size=N),
+            "user": np.random.choice(users, size=N),
+            "date": np.random.choice(dates, size=N),
+        }
+    )
+
+    df["date"] = pd.to_datetime(df["date"])
+    df["dow"] = df["date"].dt.day_name()
+
+    return df
+
+
+if __name__ == "__main__":
+    clusters = [f"Cluster {i}" for i in range(100)]
+    dates = [f"{date(2022, 1, i):%Y-%m-%d}" for i in range(1, 32)]
+    N = 10_000
+    df = generate_random_data(clusters, dates, N)
+    sw = StratifiedSwitchbackSplitter(
+        cluster_cols=["cluster", "date"], strata_cols=["dow"]
+    )
+
+    perturbator = UniformPerturbator(
+        average_effect=0.1,
+    )
+
+    analysis = PairedTTestClusteredAnalysis(
+        cluster_cols=["cluster", "date"], strata_cols=["cluster"]
+    )
+
+    pw = PowerAnalysis(
+        perturbator=perturbator, splitter=sw, analysis=analysis, n_simulations=50
+    )
+
+    print(df)
+    power = pw.power_analysis(df)
+    print(f"{power = }")
