b42f4977dc789fba51a63887c856c3959fa7df1d
David <david26694@gmail.com>
2023-03-10 11:46:02 +0100

    refactor constant washover

--- cluster_experiments/washover.py
+++ cluster_experiments/washover.py
@@ -82,104 +82,6 @@ class EmptyWashover(Washover):
         return df
 
 
-class ConstantWashover(Washover):
-    """Constant washover - we drop all rows in the washover period when
-    there is a switch where the treatment is different."""
-
-    def __init__(self, washover_time_delta: datetime.timedelta):
-        self.washover_time_delta = washover_time_delta
-
-    def washover(
-        self,
-        df: pd.DataFrame,
-        truncated_time_col: str,
-        treatment_col: str,
-        cluster_cols: List[str],
-        original_time_col: Optional[str] = None,
-    ) -> pd.DataFrame:
-        """Constant washover - removes rows after the switch.
-
-        Args:
-            df (pd.DataFrame): Input dataframe.
-            truncated_time_col (str): Name of the truncated time column.
-            treatment_col (str): Name of the treatment column.
-            cluster_cols (List[str]): List of clusters of experiment.
-            original_time_col (Optional[str], optional): Name of the original time column.
-
-        Returns:
-            pd.DataFrame: Same dataframe as input.
-
-        Usage:
-        ```python
-        from cluster_experiments import SwitchbackSplitter
-        from cluster_experiments import ConstantWashover
-
-        washover = ConstantWashover(washover_time_delta=datetime.timedelta(minutes=30))
-
-        n = 10
-        df = pd.DataFrame(
-            {
-                # Random time each minute in 2022-01-01, length 10
-                "time": pd.date_range("2022-01-01", "2022-01-02", freq="1min")[
-                    np.random.randint(24 * 60, size=n)
-                ],
-                "city": random.choices(["TGN", "NYC", "LON", "REU"], k=n),
-            }
-        )
-
-
-        splitter = SwitchbackSplitter(
-            washover=washover,
-            time_col="time",
-            cluster_cols=["city", "time"],
-            treatment_col="treatment",
-            switch_frequency="30T",
-        )
-
-        out_df = splitter.assign_treatment_df(df=washover_split_df)
-
-        """
-        # Set original time column
-        original_time_col = (
-            original_time_col
-            if original_time_col
-            else _original_time_column(truncated_time_col)
-        )
-        # Cluster columns that do not involve time
-        non_time_cols = list(set(cluster_cols) - set([truncated_time_col]))
-        # For each cluster, we need to check if treatment has changed wrt last time
-        df_agg = df.drop_duplicates(subset=cluster_cols + [treatment_col]).copy()
-        df_agg["__changed"] = (
-            df_agg.groupby(non_time_cols)[treatment_col].shift(1)
-            != df_agg[treatment_col]
-        )
-        df_agg = df_agg.loc[:, cluster_cols + ["__changed"]]
-        return (
-            df.merge(df_agg, on=cluster_cols, how="inner")
-            .assign(
-                __time_since_switch=lambda x: x[original_time_col].astype(
-                    "datetime64[ns]"
-                )
-                - x[truncated_time_col].astype("datetime64[ns]"),
-                __after_washover=lambda x: x["__time_since_switch"]
-                > self.washover_time_delta,
-            )
-            # add not changed in query
-            .query("__after_washover or not __changed")
-            .drop(columns=["__time_since_switch", "__after_washover", "__changed"])
-        )
-
-    @classmethod
-    def from_config(cls, config) -> "Washover":
-        if not config.washover_time_delta:
-            raise ValueError(
-                f"Washover time delta must be specified for ConstantWashover, while it is {config.washover_time_delta = }"
-            )
-        return cls(
-            washover_time_delta=config.washover_time_delta,
-        )
-
-
 class TwoSidedWashover(Washover):
     """Two sided washover - we drop all rows before and after the switch within
     the time delta when there is a switch where the treatment is different."""
@@ -321,6 +223,22 @@ class TwoSidedWashover(Washover):
             washover_time_delta_before=config.washover_time_delta,
             washover_time_delta_after=config.washover_time_delta,
         )
+
+
+class ConstantWashover(TwoSidedWashover):
+    """Constant washover - we drop all rows in the washover period when
+    there is a switch where the treatment is different."""
+
+    def __init__(self, washover_time_delta: datetime.timedelta):
+        super().__init__(datetime.timedelta(seconds=0), washover_time_delta)
+
+    @classmethod
+    def from_config(cls, config) -> "Washover":
+        if not config.washover_time_delta:
+            raise ValueError(
+                f"Washover time delta must be specified for SimetricWashover, while it is {config.washover_time_delta = }"
+            )
+        return cls(washover_time_delta=config.washover_time_delta)
 
 
 class SimmetricWashover(TwoSidedWashover):
--- tests/splitter/test_washover.py
+++ tests/splitter/test_washover.py
@@ -11,7 +11,7 @@ from cluster_experiments.washover import (
 )
 
 
-@pytest.mark.parametrize("minutes, n_rows", [(30, 2), (10, 4), (15, 3)])
+@pytest.mark.parametrize("minutes, n_rows", [(30, 3), (10, 4), (15, 3)])
 def test_constant_washover_base(minutes, n_rows, washover_base_df):
 
     out_df = ConstantWashover(washover_time_delta=timedelta(minutes=minutes)).washover(
@@ -23,7 +23,6 @@ def test_constant_washover_base(minutes, n_rows, washover_base_df):
     )
 
     assert len(out_df) == n_rows
-    assert (out_df["original___time"].dt.minute > minutes).all()
 
 
 @pytest.mark.parametrize("minutes, n_rows", [(30, 2), (15, 2), (12, 3), (10, 4)])
@@ -37,7 +36,6 @@ def test_simmetric_washover_base(minutes, n_rows, simmetric_washover_base_df):
     )
 
     assert len(out_df) == n_rows
-    assert (out_df["original___time"].dt.minute > minutes).all()
 
 
 @pytest.mark.parametrize(
@@ -64,8 +62,8 @@ def test_2_sided_washover(
 @pytest.mark.parametrize(
     "minutes, n_rows, df",
     [
-        (30, 4, "washover_df_no_switch"),
-        (30, 4 + 4, "washover_df_multi_city"),
+        (30, 5, "washover_df_no_switch"),
+        (30, 5 + 5, "washover_df_multi_city"),
     ],
 )
 def test_constant_washover_no_switch(minutes, n_rows, df, request):
