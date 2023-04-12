d14532d71edcd41e686a6f51344b2cf62efd5be3
David <david26694@gmail.com>
2023-04-12 10:54:03 +0200

    bump 061

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ dev_packages = test_packages + util_packages + docs_packages
 
 setup(
     name="cluster_experiments",
-    version="0.6.0",
+    version="0.6.1",
     packages=find_packages(),
     extras_require={
         "dev": dev_packages,
