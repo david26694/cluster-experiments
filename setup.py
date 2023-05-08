0d94f77296fb6aaf4e35108c7cc84fd17d2c6060
David Masip <david26694@gmail.com>
2023-05-08 16:56:35 +0200

    Remove ambiguous (#105)
    * remove ambiguous
    
    * bump 064

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ dev_packages = test_packages + util_packages + docs_packages
 
 setup(
     name="cluster_experiments",
-    version="0.6.3",
+    version="0.6.4",
     packages=find_packages(),
     extras_require={
         "dev": dev_packages,
