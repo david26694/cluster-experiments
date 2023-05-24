1e53bf30fc6ff2cd255d44f63096f82aa01b79a5
Alex Hensel <alexander.hensel@glovoapp.com>
2023-05-24 12:50:27 +0200

    Improve docstring

--- cluster_experiments/perturbator.py
+++ cluster_experiments/perturbator.py
@@ -356,7 +356,7 @@ class BetaRelativePerturbator(NormalPerturbator, RelativePositivePerturbator):
     A stochastic Perturbator for continuous targets that applies a  sampled
     effect from a scaled Beta distribution. It applies the effect multiplicatively.
 
-    The average effect is defined for values in the specified range
+    The sampled effect is defined for values in the specified range
     (range_min, range_max). It's recommended to set range_min<0 and range_max>0
     in a "symmetric way" around 0, such that log(1 + range_min) = -log(1 + range_max).
     This ensures to have an "equal range" of perturbations that relatively
