c9daa4cf139be600806ac59f501dfa747f8ccad0
Alex Hensel <alexander.hensel@glovoapp.com>
2023-05-24 13:15:09 +0200

    Revert only applying get_cluster_perturbator once

--- cluster_experiments/perturbator.py
+++ cluster_experiments/perturbator.py
@@ -586,10 +586,9 @@ class ClusteredBetaRelativePerturbator(BetaRelativePositivePerturbator):
         df = df.copy().reset_index(drop=True)
         df = self._set_cluster_col_values(df)
 
-        cluster_perturbator = self.get_cluster_perturbator(average_effect)
         df_perturbed = pd.concat(
             [
-                cluster_perturbator.perturbate(
+                self.get_cluster_perturbator(average_effect).perturbate(
                     df=df[df[self.cluster_col] == cluster].copy()
                 )
                 for cluster in df[self.cluster_col].unique()
