8805a2ae6adaf211b64f706f7220d7e3ebbcba17
David <david26694@gmail.com>
2023-05-26 11:49:37 +0200

    feat: add new version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ dev_packages = test_packages + util_packages + docs_packages
 
 setup(
     name="cluster_experiments",
-    version="0.9.0",
+    version="0.10.0",
     packages=find_packages(),
     extras_require={
         "dev": dev_packages,
