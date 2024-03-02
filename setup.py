b571420c7ccd26b6b6a5658c20b275f46959987d
brb <victor_bouzas@hotmail.com>
2024-03-02 09:17:02 +0100

    update package version

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ dev_packages = test_packages + util_packages + docs_packages
 
 setup(
     name="cluster_experiments",
-    version="0.13.0",
+    version="0.14.0",
     packages=find_packages(),
     extras_require={
         "dev": dev_packages,
