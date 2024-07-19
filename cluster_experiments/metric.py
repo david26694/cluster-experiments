bf3bf2e12cbdffe4f474227366725fa496b55abe
ludovico-lanni <102026076+ludovico-lanni@users.noreply.github.com>
2024-07-19 16:31:41 +0200

    add metric and variant classes

---
+++ cluster_experiments/metric.py
@@ -0,0 +1,55 @@
+class Metric:
+    """
+    A class used to represent a Metric with an alias and components.
+
+    Attributes
+    ----------
+    alias : str
+        A string representing the alias of the metric
+    components : tuple
+        A tuple of strings representing the components of the metric
+
+    Methods
+    -------
+    __init__(self, alias: str, components: tuple):
+        Initializes the Metric with the provided alias and components.
+    _validate_inputs(alias: str, components: tuple):
+        Validates the inputs for the Metric class.
+    """
+
+    def __init__(self, alias: str, components: tuple):
+        """
+        Parameters
+        ----------
+        alias : str
+            The alias of the metric
+        components : tuple
+            A tuple of strings representing the components of the metric
+        """
+        self._validate_inputs(alias, components)
+        self.alias = alias
+        self.components = components
+
+    @staticmethod
+    def _validate_inputs(alias: str, components: tuple):
+        """
+        Validates the inputs for the Metric class.
+
+        Parameters
+        ----------
+        alias : str
+            The alias of the metric
+        components : tuple
+            A tuple of strings representing the components of the metric
+
+        Raises
+        ------
+        TypeError
+            If the alias is not a string or if components is not a tuple of strings.
+        """
+        if not isinstance(alias, str):
+            raise TypeError("Alias must be a string")
+        if not isinstance(components, tuple) or not all(
+            isinstance(comp, str) for comp in components
+        ):
+            raise TypeError("Components must be a tuple of strings")
---
+++ cluster_experiments/variant.py
@@ -0,0 +1,53 @@
+class Variant:
+    """
+    A class used to represent a Variant with a name and a control flag.
+
+    Attributes
+    ----------
+    name : str
+        The name of the variant
+    is_control : bool
+        A boolean indicating if the variant is a control variant
+
+    Methods
+    -------
+    __init__(self, name: str, is_control: bool):
+        Initializes the Variant with the provided name and control flag.
+    _validate_inputs(name: str, is_control: bool):
+        Validates the inputs for the Variant class.
+    """
+
+    def __init__(self, name: str, is_control: bool):
+        """
+        Parameters
+        ----------
+        name : str
+            The name of the variant
+        is_control : bool
+            A boolean indicating if the variant is a control variant
+        """
+        self._validate_inputs(name, is_control)
+        self.name = name
+        self.is_control = is_control
+
+    @staticmethod
+    def _validate_inputs(name: str, is_control: bool):
+        """
+        Validates the inputs for the Variant class.
+
+        Parameters
+        ----------
+        name : str
+            The name of the variant
+        is_control : bool
+            A boolean indicating if the variant is a control variant
+
+        Raises
+        ------
+        TypeError
+            If the name is not a string or if is_control is not a boolean.
+        """
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        if not isinstance(is_control, bool):
+            raise TypeError("is_control must be a boolean")
