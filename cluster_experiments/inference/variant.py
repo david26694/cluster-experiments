85f1e61626d6ee151b1533ccce4bbf01c8e58557
ludovico-lanni <102026076+ludovico-lanni@users.noreply.github.com>
2024-07-30 16:27:59 +0200

    change validation method for variant

--- cluster_experiments/inference/variant.py
+++ cluster_experiments/inference/variant.py
@@ -12,13 +12,6 @@ class Variant:
         The name of the variant
     is_control : bool
         A boolean indicating if the variant is a control variant
-
-    Methods
-    -------
-    __post_init__(self):
-        Validates the inputs after initialization.
-    _validate_inputs(name: str, is_control: bool):
-        Validates the inputs for the Variant class.
     """
 
     name: str
@@ -28,26 +21,18 @@ class Variant:
         """
         Validates the inputs after initialization.
         """
-        self._validate_inputs(self.name, self.is_control)
+        self._validate_inputs()
 
-    @staticmethod
-    def _validate_inputs(name: str, is_control: bool):
+    def _validate_inputs(self):
         """
         Validates the inputs for the Variant class.
-
-        Parameters
-        ----------
-        name : str
-            The name of the variant
-        is_control : bool
-            A boolean indicating if the variant is a control variant
 
         Raises
         ------
         TypeError
             If the name is not a string or if is_control is not a boolean.
         """
-        if not isinstance(name, str):
+        if not isinstance(self.name, str):
             raise TypeError("Variant name must be a string")
-        if not isinstance(is_control, bool):
+        if not isinstance(self.is_control, bool):
             raise TypeError("Variant is_control must be a boolean")
