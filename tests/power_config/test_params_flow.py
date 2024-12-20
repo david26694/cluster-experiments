40c9d241da779c97b135bb2fb3d64b206c32e3bc
David <david26694@gmail.com>
2024-12-20 13:38:29 +0100

    add default test

--- tests/power_config/test_params_flow.py
+++ tests/power_config/test_params_flow.py
@@ -15,3 +15,18 @@ def test_cov_type_flows():
 
     # then
     assert power_analysis.analysis.cov_type == "HC1"
+
+
+def test_cov_type_default():
+    # given
+    config = {
+        "analysis": "ols_non_clustered",
+        "perturbator": "constant",
+        "splitter": "non_clustered",
+    }
+
+    # when
+    power_analysis = NormalPowerAnalysis.from_dict(config)
+
+    # then
+    assert power_analysis.analysis.cov_type == "HC3"
