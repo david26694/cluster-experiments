e582b753803340bd03ae8fd93196ba2f6f3d4441
David <david26694@gmail.com>
2024-12-20 13:30:13 +0100

    add test params flow

---
+++ tests/power_config/test_params_flow.py
@@ -0,0 +1,17 @@
+from cluster_experiments.power_analysis import NormalPowerAnalysis
+
+
+def test_cov_type_flows():
+    # given
+    config = {
+        "analysis": "ols_non_clustered",
+        "perturbator": "constant",
+        "splitter": "non_clustered",
+        "cov_type": "HC1",
+    }
+
+    # when
+    power_analysis = NormalPowerAnalysis.from_dict(config)
+
+    # then
+    assert power_analysis.analysis.cov_type == "HC1"
