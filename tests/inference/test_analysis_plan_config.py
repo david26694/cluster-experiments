092e9aec68eeec41be0bf13cb54c0af31113ef54
david <david26694@gmail.com>
2025-02-28 15:14:39 +0100

    variant testing

--- tests/inference/test_analysis_plan_config.py
+++ tests/inference/test_analysis_plan_config.py
@@ -3,7 +3,10 @@ import pandas as pd
 import pytest
 
 from cluster_experiments.inference.analysis_plan import AnalysisPlan
+from cluster_experiments.inference.dimension import Dimension
 from cluster_experiments.inference.hypothesis_test import HypothesisTest
+from cluster_experiments.inference.metric import SimpleMetric
+from cluster_experiments.inference.variant import Variant
 
 
 @pytest.fixture
@@ -82,6 +85,64 @@ def test_analyze_from_metrics_dict(experiment_data):
         "" "NYC",
         "LA",
     }, "The dimension values should be present in the DataFrame"
+
+
+def test_from_dict_config():
+    # ensures that we get the same object when creating from a dict or a config
+    # given
+    d = {
+        "tests": [
+            {
+                "metric": {"alias": "AOV", "name": "order_value"},
+                "analysis_type": "clustered_ols",
+                "analysis_config": {"cluster_cols": ["customer_id"]},
+                "dimensions": [{"name": "city", "values": ["NYC", "LA"]}],
+            },
+            {
+                "metric": {"alias": "delivery_time", "name": "delivery_time"},
+                "analysis_type": "clustered_ols",
+                "analysis_config": {"cluster_cols": ["customer_id"]},
+                "dimensions": [{"name": "city", "values": ["NYC", "LA"]}],
+            },
+        ],
+        "variants": [
+            {"name": "control", "is_control": True},
+            {"name": "treatment", "is_control": False},
+        ],
+        "variant_col": "experiment_group",
+        "alpha": 0.05,
+    }
+    plan = AnalysisPlan(
+        variants=[
+            Variant(name="control", is_control=True),
+            Variant(name="treatment", is_control=False),
+        ],
+        variant_col="experiment_group",
+        alpha=0.05,
+        tests=[
+            HypothesisTest(
+                metric=SimpleMetric(alias="AOV", name="order_value"),
+                analysis_type="clustered_ols",
+                analysis_config={"cluster_cols": ["customer_id"]},
+                dimensions=[Dimension(name="city", values=["NYC", "LA"])],
+            ),
+            HypothesisTest(
+                metric=SimpleMetric(alias="delivery_time", name="delivery_time"),
+                analysis_type="clustered_ols",
+                analysis_config={"cluster_cols": ["customer_id"]},
+                dimensions=[Dimension(name="city", values=["NYC", "LA"])],
+            ),
+        ],
+    )
+
+    # when
+    plan_from_config = AnalysisPlan.from_dict(d)
+
+    # then
+    assert plan.variant_col == plan_from_config.variant_col
+    assert plan.alpha == plan_from_config.alpha
+    for variant in plan.variants:
+        assert variant in plan_from_config.variants
 
 
 def test_from_dict():
